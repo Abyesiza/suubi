115ee5e938f3664ffefce9b871de32e7aa7e8baf
Joel Abyesiza <abyesizajoel@gmail.com>
2025-08-24 13:31:57 +0300

    fixed type error

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -43,7 +43,7 @@ export default function UserDropdown() {
         {/* Add extra items as needed, e.g., Profile/Settings */}
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <SignOutButton afterSignOutUrl="/">
+          <SignOutButton>
             <button className="w-full flex items-center gap-2 text-left">
               <LogOut className="w-4 h-4" />
               Sign out
